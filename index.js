c4ca015ef8ad4ac5ac197e4a80e815f00920648e
ajkelso <ajkelso@gmail.com>
2021-01-13 21:34:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// Add your functions here
-function map() {
-    
+function map(array, fn) {
+    let r = []
+    for (let i = 0; i < array.length; i++) {
+        r.push(fn(array[i]));
+    }
+    return r
+}
+
+function reduce(array, fn, start) {
+    let result = (!!start) ? start : array[0];
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        result = fn(array[i], result)
+    }
+    return result
 }
