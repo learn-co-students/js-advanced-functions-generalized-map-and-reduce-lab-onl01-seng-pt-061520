eab4fed413706191bc30192efea6f309aced472e
DeLaCruz26 <dametrik.fick@gmail.com>
2021-01-18 02:31:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, func){
+    let newArr = []
+    for(let i in array){
+        newArr.push(func(array[i]))
+    }
+    return newArr 
+}
+
+function reduce(array, func, startVal){
+    let total = (!!startVal) ? startVal : array[0]
+    let num = (!!startVal) ? 0 : 1
+
+    for(; num < array.length; num++){
+        total = func(array[num], total)
+    }
+    return total
+}
